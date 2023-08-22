9d2499f4cabca725a1b38a6d8585d4239a74baa3
ForwardingAgent <nikolaspdk@gmail.com>
2023-08-22 09:22:39 +0300

    Mixins

--- store/products/views.py
+++ store/products/views.py
@@ -9,24 +9,29 @@ from django.views.generic.list import ListView  # 7.4
 from django.views.generic.edit import CreateView  # 7.5
 
 from products.models import ProductCategory, Product, Basket
+from common.views import TitleMixin  # 7.8
+
 from users.models import User
 from django.core.paginator import Paginator
 
 
-class IndexView(TemplateView):  # 7.3 в названии лучше использовать слово View чтобы было понятно что это из Views.py
+class IndexView(TitleMixin, TemplateView):  # 7.8 TitleMixin, 7.3 в названии лучше использовать слово View чтобы было понятно что это из Views.py
     #  TemplateView наследуется от TemplateResponseMixin, ContextMixin, View
     template_name = 'products/index.html'
-
-    def get_context_data(self, **kwargs) -> Dict[str, Any]:  # get_context_data можно использовать т.к. наследуется от ContextMixin
-        context = super(IndexView, self).get_context_data(**kwargs)  # cоздает словарь | сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
-        context['title'] = 'Store'  # дополняем словарь своими данными
-        return context
+    title = 'Store'  # 7.8
 
 
-class ProductListView(ListView):  # 7.4 ListView класс отвечающий за вывод списка объектов
+    # 7.8 def get_context_data(self, **kwargs) -> Dict[str, Any]:  # get_context_data можно использовать т.к. наследуется от ContextMixin
+    #     context = super(IndexView, self).get_context_data(**kwargs)  # cоздает словарь | сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
+    #     context['title'] = 'Store'  # дополняем словарь своими данными
+    #     return context
+
+
+class ProductListView(TitleMixin, ListView):  # 7.4 ListView класс отвечающий за вывод списка объектов
     model = Product  # с какой моделью работаем
     template_name = 'products/products.html'  # с каким шаблоном работаем
     paginate_by = 3
+    title = 'Store - Каталог'  # 7.8
 
     def get_queryset(self) -> QuerySet[Any]:
         queryset = super(ProductListView, self).get_queryset()  # тут формируется queryset=Product.objects.all()
@@ -36,7 +41,7 @@ class ProductListView(ListView):  # 7.4 ListView класс отвечающий
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super(ProductListView, self).get_context_data(**kwargs)  # сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
         context['categories'] = ProductCategory.objects.all()  # добавились категории в сайдбар
-        context['title'] = 'Store'
+        # context['title'] = 'Store - Каталог'  # 7.8 перенесли в class
         return context
 
 
--- store/users/views_new.py
+++ store/users/views_new.py
@@ -4,45 +4,52 @@ from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required  # 5.5 не позволяет отрабатывать контроллеру пока не произведена авторизация (неавториз user не может добавить в корзину или зайти на страницу профайла пока не авторизирован)
 from django.views.generic.edit import CreateView, UpdateView  # 7.6
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView  # 7.7
+from django.contrib.messages.views import SuccessMessageMixin  # 7.8
+
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-class UserLoginView(LoginView):  # 7.7
+class UserLoginView(TitleMixin, LoginView):  # 7.7
     # model = ... у нас есть в settings AUTH_USER_MODEL = user.User с ней и работает LoginView
     template_name = 'users/login.html'
     form_class = UserLoginForm  # форму которую использовали для логина
     # success_url = reverse_lazy('index') не срабатывает, поэтому пропишем перенаправление в settings LOGIN_REDIRECT_URL='/'
     # ВСЁ, остальное все авторизация (с помощью username и password) и проверки происходят сами.
+    title = 'Store - Авторизация'
 
 
-class UserRegistrationView(CreateView):  # 7.6
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):  # 7.6, 7.8 SuccessMessageMixin ВСЕГДА ПИШУТСЯ ПЕРВЫМИ!!
     model = User
-    template_name = 'users/registration.html'
     form_class = UserRegistrationForm  # какую форму использовать для регистрации UserRegistrationForm
+    template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')  # куда перенаправлять
+    success_message = 'Поздравляем, вы успешно зарегистрировались!'  # 7.8 Mixins
+    title = 'Store - Регистрация'
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:  # get_context_data можно использовать т.к. наследуется от ContextMixin
-        context = super(UserRegistrationForm).get_context_data(**kwargs)  # cоздает словарь | сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
-        context['title'] = 'Store - Регистрация'  # дополняем словарь своими данными
-        return context
+    # 7.8 def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:  # get_context_data можно использовать т.к. наследуется от ContextMixin
+    #     context = super(UserRegistrationView, self).get_context_data(**kwargs)  # cоздает словарь | сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
+    #     context['title'] = 'Store - Регистрация'
+    #     return context
 
 
-class UserProfileView(UpdateView):  # 7.6
+class UserProfileView(TitleMixin, UpdateView):  # 7.6, 7.8 TitleMixin
     model = User
     form_class = UserProfileForm  # какую форму использовать для профиля UserProfileForm (форму сами делали)
     template_name = 'users/profile.html'
     # success_url = reverse_lazy('users:profile') вместо этого берем готовую ф-ю get_success_url и переопределяем (ниже) тк у нас в urls.py приходит еще и id (profile/<int:pk>/)
+    title = 'Store - Профиль'  # 7.8
 
     def get_success_url(self) -> str:
         return reverse_lazy('users:profile', args=(self.object.id))
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:  # get_context_data можно использовать т.к. наследуется от ContextMixin
         context = super(UserProfileView, self).get_context_data(**kwargs)  # cоздает словарь | сначала через super вызываем родительский метод, чтобы он выполнился, и ниже добавляем наши ключи в словарь (т.е. переопределили род.метод)
-        context['title'] = 'Store - Профиль'  # дополняем словарь своими данными
+        # 7.8 context['title'] = 'Store - Профиль'  # дополняем словарь своими данными
         context['baskets'] = Basket.objects.filter(user=self.request.user)
         return context
 
