1e89ba016893a2847b98a7b0be2a7e110ba4165b
ForwardingAgent <nikolaspdk@gmail.com>
2024-01-13 15:27:55 +0300

    new settings and d-c

--- store/store/celery.py
+++ store/store/celery.py
@@ -7,8 +7,8 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
 app = Celery('store')
-app.config_from_object('django.conf:settings', namespace='CELERY')  # senior не знает
-app.conf.broker_url = settings.CELERY_BROKER_URL  # senior
+app.config_from_object('django.conf:settings', namespace='CELERY')  # namespace='CELERY' все настройки для Celery будут искаться с префиксом CELERY_ типа CELERY_BROKER_URL или CELERY_RESULT_BACKEND
+app.conf.broker_url = settings.CELERY_BROKER_URL      # senior
 app.autodiscover_tasks()  # celery автоматич смотрел по всем папкам и иcкал tasks
 
 
